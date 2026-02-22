b11801ac64f9082b0985c8c1dcd8484d2cd9e766
Ric Ruan <ric@ruan.com>
2026-02-22 12:32:51 +0800

    base update

--- Base/Repository/register.py
+++ Base/Repository/register.py
@@ -23,7 +23,7 @@ def register_default_connection():
             blocking=False,
         ), is_default=True)
         BaseDBModel.set_default_db_connection(ConnectionManager.get_default())
-        logger.info("默认数据库连接注册成功")
+        logger.info(f"默认数据库连接注册成功 - host: {settings.mysql.host}, port: {settings.mysql.port}, database: {settings.mysql.name}, user: {settings.mysql.user}")
     except Exception as e:
         logger.warning(f"注册默认数据库连接失败，程序将以无持久化模式运行：{str(e)}")
 
@@ -43,6 +43,6 @@ def register_base_module_connection():
             maxconnections=20,
             blocking=False,
         ))
-        logger.info("基础模块数据库连接注册成功")
+        logger.info(f"基础模块数据库连接注册成功 - host: {settings.mysql.host}, port: {settings.mysql.port}, database: {settings.base_module.db_name}, user: {settings.mysql.user}")
     except Exception as e:
         logger.warning(f"注册基础模块数据库连接失败，相关功能将无法持久化：{str(e)}")
