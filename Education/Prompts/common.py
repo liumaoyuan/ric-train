8942f7d1a0d83852dcd4bfb55953d37c29bf5317
ricruan <ric@ruan.com>
2026-02-23 21:31:57 +0800

    temp

--- Education/Prompts/common.py
+++ Education/Prompts/common.py
@@ -2,5 +2,11 @@ from jinja2 import Template
 
 
 def prompt_render(prompt, params):
+    """
+    Jinja2 模板渲染
+    :param prompt: 提示词
+    :param params: 参数
+    :return:
+    """
     template = Template(prompt)
     return template.render(params)
