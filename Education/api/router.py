12ab209557d2eadff016be325104cdfdc11b9653
ricruan <ric@ruan.com>
2026-02-23 21:28:02 +0800

    router register

--- Education/api/router.py
+++ Education/api/router.py
@@ -2,4 +2,4 @@ from Education.api.core.questionApi import router as question_router
 
 
 def router_register(app):
-    app.include_router(question_router)
+    app.include_router(question_router,tags=["教育局项目"])
--- Education/frontend/register.py
+++ Education/frontend/register.py
@@ -3,18 +3,23 @@ from pathlib import Path
 
 from starlette.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
-from fastapi import Request
+from fastapi import Request, APIRouter
 
 from fastapi import FastAPI
+
 # 定位当前目录作为模板根路径 （如此 需要 .html 文件和当前文件同目录下）
 BASE_DIR = Path(__file__).parent
 
+router = APIRouter(prefix="/edu")
 
-def frontend_init(app : FastAPI):
+
+def frontend_init(app: FastAPI):
     # 模板配置
     templates = Jinja2Templates(directory=BASE_DIR)
 
-    @app.get("/", response_class=HTMLResponse)
+    @router.get("/", response_class=HTMLResponse)
     async def interview_upload_page(request: Request):
         """做题页面"""
         return templates.TemplateResponse("doQuestion.html", {"request": request})
+
+    app.include_router(router, tags=["教育局项目-前端"])
