f6a3b08007d994cbd5f177e0a411e4e1f9a2029d
刘茂源 <2549996968@qq.com>
2026-05-16 16:44:59 +0800

    删除无关模块

--- Wolin/ai/__init__.py
+++
@@ -1,4 +0,0 @@
-from .interview.nodes import iaNodes
-from WorkFlow import load_node
-
-load_node(iaNodes)
--- WorkFlow/models/nodes/normalNode.py
+++
@@ -1,9 +0,0 @@
-from WorkFlow.base.wfEnum import NodeTypeEnum
-from WorkFlow.models.nodes.baseNode import BaseNode
-
-
-class NormalNode(BaseNode):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.node_type = NodeTypeEnum.NORMAL_NODE
